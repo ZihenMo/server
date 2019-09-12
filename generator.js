e6dcdf9ee749792c2ca6aaaf61135fc763b3334d
muzimo <muzimo@gshopper.com>
2019-09-12 18:02:51 +0800

    Chore:引入faker、loadsh

---
+++ generator.js
@@ -0,0 +1,31 @@
+/*
+* 生成Json数据,该文件可使用静态Json文件代替
+*/
+
+var faker = require('faker');
+// 一个JS语言工具库
+var _ = require('lodash');
+module.exports = function() {
+
+    // 设置地区
+    faker.locale = "zh_CN";
+
+    return {
+        user: _.times(10, (index) => {
+            return {
+                id: index,
+                nickName: faker.name.findName(),
+                email: faker.internet.email(),
+                avatar: faker.image.avatar()
+            }
+        }),
+        news: _.times(100, (index) => {
+           return {
+               id: index,
+               title: faker.name.title(),
+               content: faker.random.words(),
+               image: faker.image.imageUrl()
+           }
+        })
+    }
+};
