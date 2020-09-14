3a763fd0a4d9f09a986dcc38c396b53d33bee58a
muzimo <muzimo@gshopper.com>
2020-09-14 19:51:14 +0800

    Chore: 改为纯动态数据

---
+++ app/generator.js
@@ -0,0 +1,25 @@
+/*
+* 生成Json数据,该文件可使用静态Json文件代替
+*/
+
+const faker = require('faker');
+const user_generator = require('./users.js')
+// 一个JS语言工具库
+const _ = require('lodash');
+module.exports = function () {
+// 设置地区
+    faker.locale = "zh_CN";
+    return {
+        user: _.times(100, (index) => {
+            return user_generator();
+        }),
+        news: _.times(100, (index) => {
+            return {
+                id: index,
+                title: faker.name.title(),
+                content: faker.random.words(),
+                image: faker.image.imageUrl()
+            }
+        })
+    }
+};
---
+++ app/server.js
@@ -0,0 +1,60 @@
+const jsonServer = require('json-server');
+const rules = require('./router.js');
+const server = jsonServer.create();
+const db = require('./generator');
+const router = jsonServer.router(db());
+const middleware = jsonServer.defaults();
+const rewriter = jsonServer.rewriter;
+
+server.use(middleware);
+// 自定义访问控制
+function isAuthorized(req) {
+    console.log("当前请求:" + req.url);
+    // console.log(req);
+    for (const key in rules) {
+        console.log(key);
+        if (req.url.startsWith(key) || req.url === '/user') {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isRefuseService(req) {
+    return req.url.startsWith('/gp');
+}
+
+server.use((req, res, next) => {
+    if (isAuthorized(req)) { // add your authorization logic here
+        next() // continue to JSON Server router
+    }
+    else if (isRefuseService(req)) {
+        res.sendStatus(500)
+    }
+    else {
+        res.sendStatus(401)
+    }
+});
+
+// 修改响应
+router.render = (req, res) => {
+    res.jsonp({
+        body: res.locals.data, // 包装到data下
+        message:"success"
+    });
+    // res.status(500).jsonp({
+    //     error: "error message here"
+    // })
+};
+
+// 添加路由规则,须在.use(router)前
+server.use(rewriter(rules));
+
+// 修改响应
+server.use(router);
+
+// 启动服务
+server.listen(3000, () => {
+    console.log(db);
+    console.log('JSON Server is Running!');
+});
