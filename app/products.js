ffa8e3ebf2f1ec2b0b21c1ebd276fac936e48fb4
set <set>
2020-12-16 19:56:28 +0800

    Chore: 添加nodemon热更新

--- app/products.js
+++ app/products.js
@@ -1,16 +1,13 @@
 const faker = require('faker');
-// 一个JS语言工具库
-const _ = require('lodash');
 const mock = require('mockjs')
 module.exports = function () {
 // 设置地区
     faker.locale = "zh_CN";
     return {
-        id: faker.random.uuid(),
+        skuId: faker.random.uuid(),
         name: faker.name.findName(),
         signature: mock.Random.paragraph(1, 5),
         avatar: mock.Random.image('100x100'),
-        job: faker.name.jobDescriptor(),
-        gender:  faker.name.gender()
+        job: faker.name.jobDescriptor()
     }
-};
+} ();
